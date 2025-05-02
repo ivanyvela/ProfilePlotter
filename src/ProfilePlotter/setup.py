b6622ef3228ed3211c69b002044852aed47412af
ivanyvela <tradefairyel@gmail.com>
2025-05-02 11:03:13 +0200

    minor project structure edits

---
+++ src/ProfilePlotter/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='profileplotter',
+    version='0.1.0',
+    description='Plot geophysical profiles with TEM, boreholes, and satellite background',
+    author='Ivan Vela',
+    author_email='ivan.yelamos@gmail.com',
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        'geopandas',
+        'matplotlib',
+        'numpy',
+        'pandas',
+        'rasterio',
+        'scipy',
+        'shapely',
+],
+    python_requires='>=3.10, <=3.12',
+)
